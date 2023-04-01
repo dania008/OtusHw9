cb10faa731270b1c6b3e9f427ed8906c393b135c
Даниил Михайлович Володин <dvolodin@ufs.travel>
2023-04-01 18:06:13 +0300

    Добавьте файлы проекта.

---
+++ OtusHw9/ImageDownloader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OtusHw9
+{
+    public class ImageDownloader
+    {
+        public event Action ImageStarted;
+        public event Action ImageCompleted;
+        public string _fileName { get; set; }
+        public string FileName => _fileName;
+        public async Task<bool> DownloadAsync(string remoteUri, string fileName, CancellationTokenSource cts )
+        {
+            CancellationToken token = cts.Token;
+            try
+            {
+                ImageStarted?.Invoke();
+
+                using (var myWebClient = new WebClient())
+                {
+                    token.Register(() => myWebClient.CancelAsync());
+                    if (token.IsCancellationRequested)
+                    {
+                        await myWebClient.DownloadFileTaskAsync(remoteUri, fileName).ConfigureAwait(false);
+                    }
+                }
+
+                ImageCompleted?.Invoke();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ OtusHw9/Program.cs
@@ -0,0 +1,36 @@
+﻿using OtusHw9;
+
+internal class Program
+{
+
+    static async Task Main(string[] args)
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        string remoteUri = "https://img3.fonwall.ru/o/bh/russian-siberia-khakassia-qqbs.jpg?route=mid&h=750";
+        ImageDownloader downloader = new ImageDownloader();
+        downloader.ImageCompleted += () => Console.WriteLine("Скачивание файла закончилось");
+        downloader.ImageStarted += () => Console.WriteLine("Скачивание файла началось");
+        string fileName;
+        var taskList = new List<Task>();
+        for (int i = 0; i < 10; i++)
+        {
+            fileName = $"bigimage{i}.jpg";
+            var t = Task.Run(async () => { return await downloader.DownloadAsync(remoteUri, fileName, cts); });
+            taskList.Add(t);
+        }
+        Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+        while (!taskList.All(x => x.IsCompleted))
+        {
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.A)
+            {
+                Console.WriteLine("Потоки приостановлены.");
+                cts.Cancel();
+                Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+            }
+        }
+        Task.WaitAll(taskList.ToArray());
+        Console.WriteLine("Нажмите любую клавишу для выхода");
+        Console.ReadKey();
+    }
+
+}
